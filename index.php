03628b4d91bf6460bef9a6d045d57d944c5fa033
Sebastian Plattner <sebastian.plattner@gmail.com>
2015-09-19 19:20:12 +0200

    Redirect to HTTPS

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+
+if($_SERVER["HTTPS"] != "on") {
+   header("HTTP/1.1 301 Moved Permanently");
+   header("Location: https://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"]);
+   exit();
+}
+
+
 @session_start();
 
 // Set flag that this is a parent file
