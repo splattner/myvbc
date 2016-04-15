1d288da337a4b6b840069784047e3a1a62a39a07
Sebastian Plattner <sebastian.plattner@gmail.com>
2016-04-15 09:11:19 +0200

    SSL Fix

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 
-if($_SERVER["HTTPS"] != "on" && strpos($_SERVER["HTTP_HOST"], 'localhost')) {
+if($_SERVER["HTTPS"] != "on") {
    header("HTTP/1.1 301 Moved Permanently");
    header("Location: https://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"]);
    exit();
