e3ce5d81917acb4c41b1ce526107a78135131f02
Sebastian Plattner <sebastian.plattner@gmail.com>
2016-09-18 21:59:14 +0200

    Changes to soap Interface

--- bin/class.swissvolley.php
+++ bin/class.swissvolley.php
@@ -18,27 +18,30 @@ class swissvolley
 	public function __construct() // Konstruktor
 	{
 		// Soap Verbindung �ffnen
-		$this->soap_client = new SoapClient("http://myvolley.volleyball.ch/SwissVolley.wsdl");
+		$this->soap_client = new \SoapClient("http://myvolley.volleyball.ch/SwissVolley.wsdl");
 	}
 	
 	// Funktionen
 	public function get_GamesbyTeamID($teamID) // L�dt alle Spiele des Team mit ID '$teamID' und gibt diese als Array zur�ck
 	{
-		return $this->soap_client->getGamesTeam($teamID);
+		return $this->soap_client->__soapCall("getGamesTeam", array($teamID));
 	}
 	
 	public function get_GameDetailed($gameID) // Gibt detailierte Informationen �ber das Spiel mit ID '$gameID'
 	{
-		return $this->soap_client->getGameDetailed($gameID);
+		return $this->soap_client->__soapCall("getGameDetailed", array($gameID));
 	}
 	
 	public function get_Table($groupID) // Rankliste ausgeben
 	{
-		return $this->soap_client->getTable($groupID);
+		return $this->soap_client->__soapCall("getTable", array($groupID));
 	}
 
 	public function getGamesByClub($clubID) {
-		return $this->soap_client->getGamesByClub($clubID);
+
+		return $this->soap_client->__soapCall("getGamesByClub", array($clubID));
+
+
 	}
 	
 }
