7a4ace57fb4d6f259156f3f8e92a857b150ca6a7
splattner <sebastian.plattner@gmail.com>
2019-11-02 12:35:42 +0100

    Fix set signature function

--- model/person.model.php
+++ model/person.model.php
@@ -377,7 +377,9 @@ class MPerson extends Model
         $personoldData = $personoldRS->fetch();
 
 
-        $this->setState($personID, $newState);
+        $sql = "UPDATE persons SET signature = ? WHERE id = ?";
+        $sql = $this->pdo->Prepare($sql);
+        $sql->Execute(array($newState, $personID));
 
         /* Generate Notification after */
         $personnew = new MPerson();
